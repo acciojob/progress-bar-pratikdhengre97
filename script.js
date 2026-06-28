258116dc70dc12cfff773f3c47e68e7b98e5558d
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-06-28 17:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+    if (currentActive < circles.length) {
+        currentActive++;
+        update();
+    }
+});
+
+prev.addEventListener("click", () => {
+    if (currentActive > 1) {
+        currentActive--;
+        update();
+    }
+});
+
+function update() {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add("active");
+        } else {
+            circle.classList.remove("active");
+        }
+    });
+
+    const progressWidth = ((currentActive - 1) / (circles.length - 1)) * 100;
+    progress.style.width = progressWidth + "%";
+
+    prev.disabled = currentActive === 1;
+    next.disabled = currentActive === circles.length;
+}
